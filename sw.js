38f71c0716e586083c6aa8bc2e67715225d3b991
buenasysantasmadrid <buenasysantasmadrid@gmail.com>
2026-04-18 17:36:39 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,5 @@
-const CACHE = 'fichaje-v3';
-const ASSETS = ['./index.html', './manifest.json', './logo_buenasysantas.jpg'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {})));
-  self.skipWaiting();
-});
-
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
   self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  if (e.request.url.includes('script.google.com')) return;
-  e.respondWith(
-    fetch(e.request)
-      .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE).then(c => c.put(e.request, copy));
-        return response;
-      })
-      .catch(() => caches.match(e.request))
-  );
 });
